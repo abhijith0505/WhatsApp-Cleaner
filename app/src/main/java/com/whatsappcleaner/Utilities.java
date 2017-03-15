e56e337e2a086a04545706c21965a2d0019f3a86
Abhijith <abhijith4m0505@gmail.com>
2017-03-15 10:53:13 +0530

    sorts files according to date and delete all but first

---
+++ app/src/main/java/com/whatsappcleaner/Utilities.java
@@ -0,0 +1,54 @@
+package com.whatsappcleaner;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Created by Abhijith on 15-03-2017.
+ */
+
+public class Utilities {
+    String path;
+    Context mContext;
+
+    Utilities(Context context){
+        path = Environment.getExternalStorageDirectory()
+                + "/WhatsApp/Databases/";
+        mContext = context;
+    }
+
+    int delete(){
+        int success=0;
+
+        File directory = new File(path);
+        File[] files = directory.listFiles();
+
+        Arrays.sort(files, new Comparator<File>(){
+            public int compare(File f1, File f2)
+            {
+                return Long.valueOf(f2.lastModified()).compareTo(f1.lastModified());
+            } });
+
+        if(files.length > 1){
+            Log.d("Files", "Size: "+ files.length);
+            for (int i = 1; i < files.length; i++)
+            {   boolean deleted = files[i].delete();
+                Log.d("Files", "FileName:" + files[i].getName());
+            }
+        }
+        else{
+            Toast.makeText(mContext, "You do not have unwanted chat backups", Toast.LENGTH_SHORT).show();
+        }
+
+        return success;
+    }
+
+}
+
+
